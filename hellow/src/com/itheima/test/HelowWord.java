494a46a2759390d24433ada2996369cf0e960eef
sun <335757965@qq.com>
2019-04-29 19:54:14 +0800

    添加一个项目到本地仓库

---
+++ hellow/src/com/itheima/test/HelowWord.java
@@ -0,0 +1,9 @@
+package com.itheima.test;
+
+public class HelowWord {
+    public static void main(String[] args) {
+        System.out.println("111111111111111");
+        System.out.println("111111111111111");
+        System.out.println("111111111111111");
+    }
+}
